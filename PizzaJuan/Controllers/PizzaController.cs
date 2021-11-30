4935c3f0270514860d90af403bdb6a7ea7ba6315
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-11-30 16:59:05 -0600

    HU1: Added PizzaController to attach view for the creation of the pizza

---
+++ PizzaJuan/Controllers/PizzaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public JsonParser JsonParser { get; set; }
+
+        public ActionResult CreatePizza()
+        {
+            ViewBag.Bases = 
+
+
+            return View();
+        }
+    }
+}
