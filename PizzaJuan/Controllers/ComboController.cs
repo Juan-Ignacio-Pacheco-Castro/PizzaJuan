5e31e5e643ecf060eb7435e5e25f4aa5bc72fa98
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-02 19:10:06 -0600

    HU3: Added combos controller, controller and JSON

---
+++ PizzaJuan/Controllers/ComboController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Controllers
+{
+    public class ComboController : Controller
+    {
+        public JsonParser JsonParser { get; set; }
+
+        public ComboController() {
+            JsonParser = new JsonParser();
+        }
+
+        public ActionResult ComboOptions() {
+            ViewBag.Combos = JsonParser.ParseFromJSON("Combos.json");
+            return View();
+        }
+    }
+}
