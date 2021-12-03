8ffe6ea740b2086e4e3ac6c79b5e9c25d328b6ff
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-02 18:17:23 -0600

    HU2: created controller and view to choose drink

---
+++ PizzaJuan/Controllers/DrinkController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Controllers
+{
+    public class DrinkController : Controller
+    {
+        public JsonParser JsonParser { get; set; }
+
+        public DrinkController() {
+            JsonParser = new JsonParser();
+        }
+
+        public ActionResult ChooseBeverage() {
+            ViewBag.Beverages = JsonParser.ParseFromJSON("Beverages.json");
+            return View();
+        }
+    }
+}
