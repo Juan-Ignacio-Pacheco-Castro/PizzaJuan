2c8dc05f40cac7d2334aca31ba0678966776d0d7
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-03 11:27:35 -0600

    HU4: Added Menu controller and view

---
+++ PizzaJuan/Controllers/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Controllers
+{
+    public class MenuController : Controller
+    {
+        public JsonParser JsonParser { get; set; }
+
+        public MenuController() {
+            JsonParser = new JsonParser();
+        }
+
+        public ActionResult Menu() {
+            return View();
+        }
+    }
+}
