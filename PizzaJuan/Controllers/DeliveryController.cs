137cbf636cae74379d6fea6cbd40829a3750eace
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-03 11:50:20 -0600

    HU6: Added  DeliveryController to manage the client express and takeout options

---
+++ PizzaJuan/Controllers/DeliveryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Controllers
+{
+    public class DeliveryController : JsonParserController
+    {
+        public ActionResult ComboOptions() {
+            ViewBag.Combos = JsonParser.ParseFromJSON("Combos.json");
+            return View();
+        }
+    }
+}
