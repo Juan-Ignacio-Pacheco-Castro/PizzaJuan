e5720b2044bdcd48818a48642bdd5788fa206bc5
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-04 13:18:29 -0600

    Testing: Added tests of methods in JsonParser

---
+++ PizzaJuan.Tests/Controllers/JsonParserTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Tests.Controllers {
+    [TestClass]
+    public class JsonParserTest {
+        [TestMethod]
+        public void TestMethodGetOrderFromJsonInJsonParser() {
+            // Arrange
+            JsonParser parser = new JsonParser();
+
+            // Act
+            string json = "[{\"Description\": \"Pizza Jamon Grande con extra de tomate\",\"Price\": 2000}]";
+            dynamic jsonCollection = JsonConvert.DeserializeObject(json);
+            List<ProductModel> productModels = parser.GetOrderFromJson(jsonCollection);
+
+            // Assert
+            Assert.IsNotNull(productModels);
+            Assert.AreEqual(productModels.Count, 1);
+            Assert.AreEqual(productModels.First().Price, 2000);
+        }
+
+        [TestMethod]
+        public void TestMethodJoinNewDataInJsonParser() {
+            // Arrange
+            JsonParser parser = new JsonParser();
+            ProductModel product = new ProductModel();
+            product.Price = 3000;
+            product.Description = "Hello world";
+
+            // Act
+            string jsonString = parser.JoinNewData<ProductModel>("Order.json", product, parser.GetOrderFromJson);
+
+            // Assert
+            string result = "[{\"Description\": \"Pizza Jamon Grande con extra de tomate\",\"Price\": 2000},{\"Description\": \"Hello world\",\"Price\": 3000}]";
+            Assert.IsNotNull(jsonString);
+            Assert.AreEqual(result, jsonString);
+        }
+
+        [TestMethod]
+        public void TestMethodExtractRawContentInJsonParser() {
+            // Arrange
+            JsonParser parser = new JsonParser();
+            PrivateObject obj = new PrivateObject(parser);
+
+            // Act
+            var retVal = obj.Invoke("ExtractRawContent", "Order.json");
+
+            // Assert
+            string result = "[{\"Description\": \"Pizza Jamon Grande con extra de tomate\"}]";
+            Assert.IsNotNull(retVal);
+            Assert.AreEqual(result, retVal);
+        }
+    }
+}
---
+++ PizzaJuan.Tests/Controllers/PizzaControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzaJuan.Controllers;
+using PizzaJuan.Models;
+
+namespace PizzaJuan.Tests.Controllers {
+    [TestClass]
+    public class PizzaControllerTest {
+        [TestMethod]
+        public void TestPostCreatePizza() {
+            // Arrange
+            PizzaController controller = new PizzaController();
+            ProductModel product = new ProductModel();
+            JsonParser parser = new JsonParser();
+            product.Price = 2000;
+            product.Description = "Combos de los Tios";
+
+            // Act
+            //controller.PostCreatePizza(product);
+            parser.WriteToJsonFile<ProductModel>("Order.json", product, parser.GetOrderFromJson);
+
+            // Assert
+            //Assert.AreEqual(parser.ParseFromJSON("Order.json")["Price"], 2000);
+        }
+    }
+}
