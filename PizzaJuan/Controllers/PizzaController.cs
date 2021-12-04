bfaa867709e4ec746888fe01ccc924181f7968c8
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-04 11:38:20 -0600

    HU4: Adding elements to order

--- PizzaJuan/Controllers/PizzaController.cs
+++ PizzaJuan/Controllers/PizzaController.cs
@@ -1,12 +1,18 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using PizzaJuan.Models;
 
 namespace PizzaJuan.Controllers
 {
     public class PizzaController : JsonParserController
     {
+        
         public ActionResult CreatePizza()
         {
+            HttpCookie userInfo = new HttpCookie("userInfo");
+            userInfo["UserName"] = "Annathurai";
+            userInfo["UserColor"] = "Black";
+            Response.Cookies.Add(userInfo);
             ViewBag.Bases = JsonParser.ParseFromJSON("Bases.json");
             ViewBag.Sizes = JsonParser.ParseFromJSON("Sizes.json");
             ViewBag.Ingredients = JsonParser.ParseFromJSON("Ingredients.json");
---
+++ PizzaJuan/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaJuan.Models {
+    public class ProductModel {
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
